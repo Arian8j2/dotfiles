db715055b922a8a543af46739f73470eee750550
Arian Rezazadeh <notarian.rez@gmail.com>
2022-11-06 00:04:48 +0330

    nvim: add keymap for float terminal

--- config/nvim/lua/myconfigs/utils.lua
+++ config/nvim/lua/myconfigs/utils.lua
@@ -32,7 +32,8 @@ require("toggleterm").setup({
     size = 15,
 })
 vim.keymap.set("t", "<A-c>", "<C-\\><C-n>")
-vim.keymap.set({"n", "t"}, "<C-j>", "<cmd>ToggleTerm<enter>")
+vim.keymap.set({"n", "t"}, "<C-j>", "<cmd>ToggleTerm direction=horizontal<enter>")
+vim.keymap.set({"n", "t"}, "<C-f>", "<cmd>ToggleTerm direction=float<enter>")
 
 -- cheat sh
 vim.keymap.set("n", "<leader>fc", "<cmd>Cheat<enter>")
