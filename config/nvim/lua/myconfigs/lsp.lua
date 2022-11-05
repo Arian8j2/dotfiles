1398ae50f7fde5fd5432b3816e13f0ae2b24064f
Arian Rezazadeh <notarian.rez@gmail.com>
2022-11-05 23:51:42 +0330

    nvim: rollback table.unpack to unpack

--- config/nvim/lua/myconfigs/lsp.lua
+++ config/nvim/lua/myconfigs/lsp.lua
@@ -46,7 +46,7 @@ require("mason-lspconfig").setup_handlers {
 -- auto completion
 local snippy = require("snippy")
 local has_words_before = function()
-    local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
