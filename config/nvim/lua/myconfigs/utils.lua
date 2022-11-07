f9e28607314e3d3e1c74a8c5f8a3e02909342995
Arian Rezazadeh <notarian.rez@gmail.com>
2022-11-07 15:51:41 +0330

    nvim: add git stage hunk and undo stage hunk

--- config/nvim/lua/myconfigs/utils.lua
+++ config/nvim/lua/myconfigs/utils.lua
@@ -39,8 +39,9 @@ require("gitsigns").setup({
         local gs = package.loaded.gitsigns
         local opts = { buffer = bufnr }
 
-        vim.keymap.set({"n", "v"}, "<leader>hs", ":Gitsigns stage_hunk<CR>", opts)
+        vim.keymap.set({"n", "v"}, "<leader>ha", ":Gitsigns stage_hunk<CR>", opts)
         vim.keymap.set({"n", "v"}, "<leader>hr", ":Gitsigns reset_hunk<CR>", opts)
+        vim.keymap.set("n", "<leader>hu", gs.undo_stage_hunk, opts)
         vim.keymap.set("n", "<leader>hp", gs.preview_hunk, opts)
         vim.keymap.set("n", "<leader>hb", function() gs.blame_line({ full=true }) end, opts)
         vim.keymap.set("n", "<leader>hd", gs.diffthis, opts)
