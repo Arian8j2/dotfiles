96a88f6ece572eeaa7a9db9da6a492384790f060
Arian Rezazadeh <notarian.rez@gmail.com>
2022-12-03 00:39:01 +0330

    nvim: avoid scrolling when switching buffers

--- config/nvim/lua/myconfigs/utils.lua
+++ config/nvim/lua/myconfigs/utils.lua
@@ -61,3 +61,33 @@ vim.keymap.set("n", "<leader>fc", "<cmd>Cheat<enter>")
 
 -- colorizer
 require("colorizer").setup({"*"}, { RGB = false, names = false })
+
+-- avoid scrolling when switching buffers
+vim.api.nvim_exec([[
+" Save current view settings on a per-window, per-buffer basis.
+function! AutoSaveWinView()
+    if !exists("w:SavedBufView")
+        let w:SavedBufView = {}
+    endif
+    let w:SavedBufView[bufnr("%")] = winsaveview()
+endfunction
+
+" Restore current view settings.
+function! AutoRestoreWinView()
+    let buf = bufnr("%")
+    if exists("w:SavedBufView") && has_key(w:SavedBufView, buf)
+        let v = winsaveview()
+        let atStartOfFile = v.lnum == 1 && v.col == 0
+        if atStartOfFile
+            call winrestview(w:SavedBufView[buf])
+        endif
+        unlet w:SavedBufView[buf]
+    endif
+endfunction
+
+" When switching buffers, preserve window view.
+if v:version >= 700
+    autocmd BufLeave * call AutoSaveWinView()
+    autocmd BufEnter * call AutoRestoreWinView()
+endif
+]], true)
