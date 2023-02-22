21efe7a9a7c53ee0b6819030dda18e58a70dcbd4
Arian Rezazadeh <notarian.rez@gmail.com>
2023-02-22 10:46:34 +0330

    nvim: add telescope spell suggest

--- config/nvim/lua/myconfigs/telescope.lua
+++ config/nvim/lua/myconfigs/telescope.lua
@@ -1,4 +1,5 @@
 local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>fl", builtin.spell_suggest, {})
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
 vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
