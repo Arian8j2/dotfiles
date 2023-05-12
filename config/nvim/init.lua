9224eb8dec33126217e8918c50f436343828ab79
Arian Rezazadeh <notarian.rez@gmail.com>
2023-05-12 16:50:18 +0330

    nvim: remove select ease plugin

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -39,7 +39,6 @@ require("packer").startup(function(use)
     use("JASONews/glow-hover.nvim")
     use("johnfrankmorgan/whitespace.nvim")
     use("j-hui/fidget.nvim")
-    use("ziontee113/SelectEase")
 
     -- status line
     use("nvim-lualine/lualine.nvim")
