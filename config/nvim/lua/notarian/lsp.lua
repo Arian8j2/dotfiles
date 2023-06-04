ba122e644239e5ddf3aee64b9c3e65b4a7ed3da4
Arian Rezazadeh <notarian.rez@gmail.com>
2023-06-04 11:33:52 +0330

    nvim: uptime all plugins, lsp servers

--- config/nvim/lua/notarian/lsp.lua
+++ config/nvim/lua/notarian/lsp.lua
@@ -33,20 +33,24 @@ require("mason-lspconfig").setup_handlers({
         })
     end,
 
-    ["sumneko_lua"] = function()
-        require("lspconfig").sumneko_lua.setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
+    ["lua_ls"] = function()
+        require("lspconfig").lua_ls.setup({
             settings = {
                 Lua = {
+                    runtime = {
+                        version = "LuaJIT",
+                    },
                     diagnostics = {
-                        globals = { "vim" },
+                        globals = {"vim"},
                     },
                     workspace = {
                         library = vim.api.nvim_get_runtime_file("", true),
                     },
-                }
-            }
+                    telemetry = {
+                        enable = false,
+                    },
+                },
+            },
         })
     end
 })
