341c0ee0585d28b89e1a5d69a09d7e98f5ca77bb
Arian Rezazadeh <notarian.rez@gmail.com>
2024-09-26 00:19:27 +0330

    fix(nvim): conform plugin issue of handling lsp formatter first

--- config/nvim/after/plugin/conform.rc.lua
+++ config/nvim/after/plugin/conform.rc.lua
@@ -2,12 +2,11 @@ require("conform").setup({
     formatters_by_ft = {
         python = { "ruff_format" },
         typescriptreact = { "prettier" },
-
-        -- use lsp formatter for other filetypes
-        ["_"] = {
-            lsp_format = "prefer"
-        }
+        toml = { "taplo" },
+    },
+    default_format_opts = {
+        lsp_format = "prefer",
     },
 })
 
-vim.keymap.set("n", "<leader>lf", require("conform").format)
+vim.keymap.set({"n", "v"}, "<leader>lf", require("conform").format)
