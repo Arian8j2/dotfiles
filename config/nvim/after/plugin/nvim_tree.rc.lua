16a2e5c89f44f279714d593defb6cac330935f19
Arian Rezazadeh <notarian.rez@gmail.com>
2024-09-26 00:20:04 +0330

    fix(nvim): don't close nvim when only nerdtree window exists

--- config/nvim/after/plugin/nvim_tree.rc.lua
+++ config/nvim/after/plugin/nvim_tree.rc.lua
@@ -16,15 +16,5 @@ require("nvim-tree").setup({
     }
 })
 
--- auto close nvim tree when is only buffer left
-vim.api.nvim_create_autocmd("BufEnter", {
-    nested = true,
-    callback = function()
-        if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
-            vim.cmd "quit"
-        end
-    end
-})
-
 vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<enter>", { silent = true })
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<enter>", { silent = true })
