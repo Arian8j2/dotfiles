b610062c6e619d481ecd54f76acb1b9d33c34268
Arian Rezazadeh <notarian.rez@gmail.com>
2024-12-20 23:57:34 +0330

    fix(waybar): usdt_price shell script crashing when nobitex api fails

--- config/waybar/scripts/usdt_price.sh
+++ config/waybar/scripts/usdt_price.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-nobitex_result=$(curl -fSs --compressed 'https://api.nobitex.ir/v2/orderbook/USDTIRT') || return 1
+nobitex_result=$(timeout 10 curl -fSs --compressed 'https://api.nobitex.ir/v2/orderbook/USDTIRT') || exit 1
 usdt_rial_price=$(echo "$nobitex_result" | jq '.bids.[0].[0] | tonumber')
 
 # sometimes nobitex returns null
 if [ -z "$usdt_rial_price" ]; then
-    return 1
+    exit 1
 fi
 
 usdt_toman_price=$(($usdt_rial_price / 100))
