ea79bda5331fb16baac227fcd79a4a231e42a993
Arian Rezazadeh <notarian.rez@gmail.com>
2025-05-12 16:52:46 +0330

    refactor(nvim): indent o matic configuration to my new fork

--- config/nvim/after/plugin/indent_o_matic.rc.lua
+++ config/nvim/after/plugin/indent_o_matic.rc.lua
@@ -4,24 +4,26 @@ vim.bo.expandtab = true
 
 require("indent-o-matic").setup({
     max_lines = 1024,
-    forced_default_width = 4,
+    default_width = 4,
     standard_widths = { 2, 4, 8 },
     skip_multiline = true,
 
     filetype_typescriptreact = {
-        forced_default_width = 2
+        default_width = 2
     },
 
     filetype_typesript = {
-        forced_default_width = 2
+        default_width = 2
     },
 
     filetype_yaml = {
-        forced_default_width = 2,
-        standard_widths = { 2 }
+        forced_width = 2
     },
 
-    filetype_nix = {
-        forced_default_width = 2
-    },
+    filetype_make = {
+        forced_width = 4,
+        after_indent_callback = function()
+            vim.bo.expandtab = false
+        end
+    }
 })
