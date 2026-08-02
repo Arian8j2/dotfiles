ef81036ccb451022061577bdb2ba862a978f1359
Arian Rezazadeh <notarian.rez@gmail.com>
2026-08-02 18:03:20 +0330

    feat(nvim): add more keybinds for diffview plugins

--- config/nvim/lua/plugins/diffview.lua
+++ config/nvim/lua/plugins/diffview.lua
@@ -5,7 +5,9 @@ return {
     },
     keys = {
         { "<leader>do", "<cmd>DiffviewOpen<enter>" },
+        { "<leader>ds", "<cmd>DiffviewOpen --staged<enter>" },
         { "<leader>dc", "<cmd>DiffviewClose<enter>" },
+        { "<leader>de", "<cmd>DiffviewToggleFiles<enter>" },
     },
     init = function()
         vim.opt.fillchars = vim.opt.fillchars + 'diff:╱'
